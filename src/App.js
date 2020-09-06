e175ffc44e6a08a88e752dabbfa41468df13f9f2
bekir16 <bekirburak.aslan16@gmail.com>
2020-09-07 00:02:32 +0300

    redundant imports deleted

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SingUp from './pages/SignUp';
 import Table from './components/Table'
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 function App() {
   return (
